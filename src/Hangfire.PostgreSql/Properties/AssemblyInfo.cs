2c108e2025b2a08357ff9ec0851d6059cf1fdf43
frankhommers <frank-github@hommers.nl>
2014-11-06 21:55:39 +0100

    Changed version numbers before NuGet push

--- src/Hangfire.PostgreSql/Properties/AssemblyInfo.cs
+++ src/Hangfire.PostgreSql/Properties/AssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
