d2bb0ac7be1c9258b2c7dda543e6609eeb8d1d24
frankhommers <frank-github@hommers.nl>
2014-11-06 14:08:22 +0100

    Added NuGet package build

--- tests/Hangfire.PostgreSql.Tests/Utils/PostgreSqlTestObjectsInitializer.cs
+++ tests/Hangfire.PostgreSql.Tests/Utils/PostgreSqlTestObjectsInitializer.cs
@@ -1,18 +1,23 @@
-﻿// This file is part of Hangfire.
-// Copyright © 2013-2014 Sergey Odinokov.
+﻿// This file is part of Hangfire.PostgreSql.
+// Copyright © 2014 Frank Hommers <http://hmm.rs/Hangfire.PostgreSql>.
 // 
-// Hangfire is free software: you can redistribute it and/or modify
+// Hangfire.PostgreSql is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as 
 // published by the Free Software Foundation, either version 3 
 // of the License, or any later version.
 // 
-// Hangfire is distributed in the hope that it will be useful,
+// Hangfire.PostgreSql  is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 // 
 // You should have received a copy of the GNU Lesser General Public 
-// License along with Hangfire. If not, see <http://www.gnu.org/licenses/>.
+// License along with Hangfire.PostgreSql. If not, see <http://www.gnu.org/licenses/>.
+//
+// This work is based on the work of Sergey Odinokov, author of 
+// Hangfire. <http://hangfire.io/>
+//   
+//    Special thanks goes to him.
 
 using System;
 using System.Diagnostics.CodeAnalysis;
