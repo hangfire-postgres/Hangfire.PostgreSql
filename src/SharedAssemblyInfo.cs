f2d8563f9eef693468898b7a2b4d513824b63abf
frankhommers <frank-github@hommers.nl>
2015-01-21 14:36:42 +0100

    Bumped version and removed unused dependencies.

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyInformationalVersion("1.3.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
