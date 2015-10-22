31f93a83f20eca4671bcd27365063f73bd8308b5
Viktor Svyatokha <ahydrax@gmail.com>
2015-10-22 16:18:45 +1000

    Added migration for Hangfire >=1.5
    Due to new format of server name in Hangfire
    (https://github.com/HangfireIO/Hangfire/blob/master/src/Hangfire.Core/Server/BackgroundProcessingServer.cs#L166)
    the old size of server.id column is not applicable for storing such
    strings.

---
+++ src/Hangfire.PostgreSql/Install.v5.sql
@@ -0,0 +1,17 @@
+﻿SET search_path = 'hangfire';
+--
+-- Table structure for table `Schema`
+--
+
+DO
+$$
+BEGIN
+    IF EXISTS (SELECT 1 FROM "schema" WHERE "version" = '5') THEN
+        RAISE EXCEPTION 'version-already-applied';
+    END IF;
+END
+$$;
+
+UPDATE "schema" SET "version" = '5' WHERE "version" = '4';
+
+ALTER TABLE server ALTER COLUMN id TYPE varchar(64);
