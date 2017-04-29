a91a5ea6a26b295dbf7b4be6b7e647518ca45667
Viktor Svyatokha <ahydrax@gmail.com>
2017-04-29 22:09:55 +1000

    Optimized counter table

---
+++ src/Hangfire.PostgreSql/Install.v8.sql
@@ -0,0 +1,17 @@
+﻿SET search_path = 'hangfire';
+--
+-- Table structure for table `Schema`
+--
+
+DO
+$$
+BEGIN
+    IF EXISTS (SELECT 1 FROM "schema" WHERE "version"::integer >= 8) THEN
+        RAISE EXCEPTION 'version-already-applied';
+    END IF;
+END
+$$;
+DROP INDEX "ix_hangfire_counter_expireat";
+ALTER TABLE "counter" ALTER COLUMN value TYPE integer;
+ALTER TABLE "counter" DROP COLUMN expireat RESTRICT;
+ALTER TABLE "counter" DROP COLUMN updatecount RESTRICT;
