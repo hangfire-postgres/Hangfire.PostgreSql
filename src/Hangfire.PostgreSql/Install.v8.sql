d1301f1863bac8bf3e7a947c697ec6addc6868d0
Viktor Svyatokha <ahydrax@gmail.com>
2017-05-02 16:28:49 +1000

    Implemeted counters aggregation

---
+++ src/Hangfire.PostgreSql/Install.v8.sql
@@ -0,0 +1,16 @@
+﻿SET search_path = 'hangfire';
+--
+-- Table structure for table `Schema`
+--
+
+DO
+$$
+BEGIN
+    IF EXISTS (SELECT 1 FROM "schema" WHERE "version"::integer >= 7) THEN
+        RAISE EXCEPTION 'version-already-applied';
+    END IF;
+END
+$$;
+
+ALTER TABLE "counter" ALTER COLUMN value TYPE bigint;
+ALTER TABLE "counter" DROP COLUMN updatecount RESTRICT;
