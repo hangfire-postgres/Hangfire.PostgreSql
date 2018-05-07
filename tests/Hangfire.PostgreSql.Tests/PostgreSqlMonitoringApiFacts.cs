41300c42a77c86b2c04bc163b58b5d0debeca819
Luís Barbosa <bymyslf@users.noreply.github.com>
2018-05-07 18:32:55 +0100

    Fix KeyNotFoundException (#81)
    * Fix KeyNotFoundException

---
+++ tests/Hangfire.PostgreSql.Tests/PostgreSqlMonitoringApiFacts.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using Hangfire.Common;
+using Hangfire.States;
+using Hangfire.Storage;
+using Moq;
+using Npgsql;
+using Xunit;
+
+namespace Hangfire.PostgreSql.Tests
+{
+    public class PostgreSqlMonitoringApiFacts
+    {
+        private readonly PersistentJobQueueProviderCollection _queueProviders;
+        private readonly PostgreSqlStorageOptions _options;
+        private readonly PostgreSqlStorage _storage;
+
+        public PostgreSqlMonitoringApiFacts()
+        {
+            var defaultProvider = new Mock<IPersistentJobQueueProvider>();
+            defaultProvider.Setup(x => x.GetJobQueue(It.IsNotNull<IDbConnection>()))
+                .Returns(new Mock<IPersistentJobQueue>().Object);
+
+            _queueProviders = new PersistentJobQueueProviderCollection(defaultProvider.Object);
+            _options = new PostgreSqlStorageOptions()
+            {
+                PrepareSchemaIfNecessary = false,
+                SchemaName = ConnectionUtils.GetSchemaName()
+            };
+
+            _storage = new PostgreSqlStorage(ConnectionUtils.GetConnectionString(), _options);
+        }
+
+        [Fact, CleanDatabase]
+        public void GetJobs_MixedCasing_ReturnsJob()
+        {
+            string arrangeSql = @"
+                insert into """ + ConnectionUtils.GetSchemaName() + @""".""job"" (""invocationdata"", ""arguments"", ""createdat"")
+                values (@invocationData, @arguments, now() at time zone 'utc') returning ""id""";
+
+            var job = Job.FromExpression(() => SampleMethod("Hello"));
+            var invocationData = InvocationData.Serialize(job);
+
+            UseConnection(sql =>
+            {
+                var jobId = sql.Query(arrangeSql, 
+                    new 
+                    {
+                        invocationData = JobHelper.ToJson(invocationData),
+                        arguments = invocationData.Arguments,
+                    }).Single().id.ToString();
+
+                var state = new Mock<IState>();
+                state.Setup(x => x.Name).Returns(SucceededState.StateName);
+                state.Setup(x => x.SerializeData())
+                    .Returns(new Dictionary<string, string>
+                    {
+                        { "SUCCEEDEDAT", "2018-05-03T13:28:18.3939693Z" },
+                        { "PerformanceDuration", "53" },
+                        { "latency", "6730"}
+                    });
+
+                Commit(sql, x => x.SetJobState(jobId, state.Object));
+
+                var monitoringApi = _storage.GetMonitoringApi();
+                var jobs = monitoringApi.SucceededJobs(0, 10);
+
+                Assert.NotNull(jobs);
+            });
+        }
+
+        private void UseConnection(Action<NpgsqlConnection> action)
+        {
+            using (var connection = ConnectionUtils.CreateConnection())
+            {
+                action(connection);
+            }
+        }
+
+        private void Commit(
+            NpgsqlConnection connection,
+            Action<PostgreSqlWriteOnlyTransaction> action)
+        {
+            using (var transaction = new PostgreSqlWriteOnlyTransaction(connection, _options, _queueProviders))
+            {
+                action(transaction);
+                transaction.Commit();
+            }
+        }
+
+        public static void SampleMethod(string arg)
+        {
+        }
+    }
+}
