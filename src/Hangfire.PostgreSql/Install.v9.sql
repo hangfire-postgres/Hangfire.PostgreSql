e394d7373106af267e5bbe7f7d78897327513713
Mathieu Poussin <git@mpouss.in>
2018-06-12 23:20:35 +0200

    Set lock resource column to TEXT type

---
+++ src/Hangfire.PostgreSql/Install.v9.sql
@@ -0,0 +1,15 @@
+SET search_path = 'hangfire';
+--
+-- Table structure for table `Schema`
+--
+
+DO
+$$
+BEGIN
+    IF EXISTS (SELECT 1 FROM "schema" WHERE "version"::integer >= 8) THEN
+        RAISE EXCEPTION 'version-already-applied';
+    END IF;
+END
+$$;
+
+ALTER TABLE "lock" ALTER COLUMN "resource" TYPE TEXT;
