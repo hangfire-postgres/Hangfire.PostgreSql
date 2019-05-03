b4bf17014a0e560de2634e81e887ff9fe9b7bfbe
Vytautas Kasparavičius <vytautas.kasparavicius@softdent.lt>
2019-05-03 21:02:41 +0300

    Fix for class file license headers missing / with some typos

--- src/Hangfire.PostgreSql/PostgreSqlDistributedLock.cs
+++ src/Hangfire.PostgreSql/PostgreSqlDistributedLock.cs
@@ -1,5 +1,5 @@
 // This file is part of Hangfire.PostgreSql.
-// Copyright � 2014 Frank Hommers <http://hmm.rs/Hangfire.PostgreSql>.
+// Copyright © 2014 Frank Hommers <http://hmm.rs/Hangfire.PostgreSql>.
 // 
 // Hangfire.PostgreSql is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as 
