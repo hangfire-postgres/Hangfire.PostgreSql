b9c73da976b37e94e8fd47df58690b15625180a6
Ilya Chumakov <ilya-chumakov@users.noreply.github.com>
2019-08-13 21:48:01 +0300

    Altering all VARCHAR(n) fields to TEXT (#121)
    * altering all VARCHAR(n) fields to TEXT

---
+++ src/Hangfire.PostgreSql/Scripts/Install.v12.sql
@@ -0,0 +1,23 @@
+SET search_path = 'hangfire';
+
+
+
+DO
+$$
+BEGIN
+  IF EXISTS (SELECT 1 FROM "schema" WHERE "version"::integer >= 12) THEN
+    RAISE EXCEPTION 'version-already-applied';
+  END IF;
+END
+$$;
+
+ALTER TABLE hangfire.counter ALTER COLUMN "key" TYPE TEXT;
+ALTER TABLE hangfire.hash ALTER COLUMN "key" TYPE TEXT;
+ALTER TABLE hangfire.hash ALTER COLUMN field TYPE TEXT;
+ALTER TABLE hangfire.job ALTER COLUMN statename TYPE TEXT;
+ALTER TABLE hangfire.list ALTER COLUMN "key" TYPE TEXT;
+ALTER TABLE hangfire.server ALTER COLUMN id TYPE TEXT;
+ALTER TABLE hangfire.set ALTER COLUMN "key" TYPE TEXT;
+ALTER TABLE hangfire.jobparameter ALTER COLUMN "name" TYPE TEXT;
+ALTER TABLE hangfire.state ALTER COLUMN "name" TYPE TEXT;
+ALTER TABLE hangfire.state ALTER COLUMN reason TYPE TEXT;
