020a8a3f96cce0f32d62ebba3c70b8548e8f8f8c
Andrew Armstrong <phplasma@gmail.com>
2021-12-27 10:17:28 +1100

    Change all "timestamp without time zone" (timestamp) columns to "timestamp with time zone" (timestamptz) to align with Npgsql v6 DateTime Local vs UTC handling

---
+++ src/Hangfire.PostgreSql/Scripts/Install.v16.sql
@@ -0,0 +1,23 @@
+﻿SET search_path = 'hangfire';
+
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT 1 FROM "schema" WHERE "version"::integer >= 16) THEN
+            RAISE EXCEPTION 'version-already-applied';
+        END IF;
+    END
+$$;
+
+ALTER TABLE counter ALTER COLUMN expireat TYPE timestamptz;
+ALTER TABLE hash ALTER COLUMN expireat TYPE timestamptz;
+ALTER TABLE job ALTER COLUMN createdat TYPE timestamptz;
+ALTER TABLE job ALTER COLUMN expireat TYPE timestamptz;
+ALTER TABLE jobqueue ALTER COLUMN fetchedat TYPE timestamptz;
+ALTER TABLE list ALTER COLUMN expireat TYPE timestamptz;
+ALTER TABLE lock ALTER COLUMN acquired TYPE timestamptz;
+ALTER TABLE server ALTER COLUMN lastheartbeat TYPE timestamptz;
+ALTER TABLE "set" ALTER COLUMN expireat TYPE timestamptz;
+ALTER TABLE state ALTER COLUMN createdat TYPE timestamptz;
+
+RESET search_path;
