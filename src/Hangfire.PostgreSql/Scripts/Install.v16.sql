92fb7a60160f4d774e39a58c32eb1665be8677df
Andrew Armstrong <phplasma@gmail.com>
2021-12-27 14:16:56 +1100

    Change remaining sequences from integer to bigint to match job_id_seq being bigint
    See https://github.com/frankhommers/Hangfire.PostgreSql/issues/192

---
+++ src/Hangfire.PostgreSql/Scripts/Install.v16.sql
@@ -0,0 +1,21 @@
+﻿SET search_path = 'hangfire';
+
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT 1 FROM "schema" WHERE "version"::integer >= 16) THEN
+            RAISE EXCEPTION 'version-already-applied';
+        END IF;
+    END
+$$;
+
+-- Note: job_id_seq is already bigint as per migration script v14
+ALTER SEQUENCE counter_id_seq AS bigint;
+ALTER SEQUENCE hash_id_seq AS bigint;
+ALTER SEQUENCE jobparameter_id_seq AS bigint;
+ALTER SEQUENCE jobqueue_id_seq AS bigint;
+ALTER SEQUENCE list_id_seq AS bigint;
+ALTER SEQUENCE set_id_seq AS bigint;
+ALTER SEQUENCE state_id_seq AS bigint;
+
+RESET search_path;
