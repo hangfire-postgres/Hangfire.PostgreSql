6061ec6e2ea9c5295ef69e2ad7eaeda0271fe04e
Žygimantas Arūna <zygimantas.aruna@visma.com>
2022-02-07 15:24:07 +0200

    Added minimum value for queue poll interval with a way to override

--- tests/Hangfire.PostgreSql.Tests/PostgreSqlStorageOptionsFacts.cs
+++ tests/Hangfire.PostgreSql.Tests/PostgreSqlStorageOptionsFacts.cs
@@ -8,7 +8,7 @@ namespace Hangfire.PostgreSql.Tests
     [Fact]
     public void Ctor_SetsTheDefaultOptions()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
 
       Assert.True(options.QueuePollInterval > TimeSpan.Zero);
       Assert.True(options.InvisibilityTimeout > TimeSpan.Zero);
@@ -17,23 +17,33 @@ namespace Hangfire.PostgreSql.Tests
     }
 
     [Fact]
-    public void Set_QueuePollInterval_ShouldThrowAnException_WhenGivenIntervalIsEqualToZero()
+    public void Set_QueuePollInterval_ShouldThrowAnException_WhenGivenIntervalIsTooLow()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
-      Assert.Throws<ArgumentException>(() => options.QueuePollInterval = TimeSpan.Zero);
+      PostgreSqlStorageOptions options = new();
+      Assert.Throws<ArgumentException>(() => options.QueuePollInterval = TimeSpan.FromMilliseconds(10));
     }
 
     [Fact]
-    public void Set_QueuePollInterval_ShouldThrowAnException_WhenGivenIntervalIsNegative()
+    public void Set_QueuePollInterval_SetsTheValue_WhenGivenIntervalIsTooLow_ButIgnored()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
-      Assert.Throws<ArgumentException>(() => options.QueuePollInterval = TimeSpan.FromSeconds(-1));
+      PostgreSqlStorageOptions options = new() {
+        AllowUnsafeValues = true,
+        QueuePollInterval = TimeSpan.FromMilliseconds(10),
+      };
+      Assert.Equal(TimeSpan.FromMilliseconds(10), options.QueuePollInterval);
+    }
+
+    [Fact]
+    public void Set_QueuePollInterval_ShouldThrowAnException_WhenGivenIntervalIsEqualToZero_EvenIfIgnored()
+    {
+      PostgreSqlStorageOptions options = new() { AllowUnsafeValues = true };
+      Assert.Throws<ArgumentException>(() => options.QueuePollInterval = TimeSpan.Zero);
     }
 
     [Fact]
     public void Set_QueuePollInterval_SetsTheValue()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
       options.QueuePollInterval = TimeSpan.FromSeconds(1);
       Assert.Equal(TimeSpan.FromSeconds(1), options.QueuePollInterval);
     }
@@ -41,21 +51,21 @@ namespace Hangfire.PostgreSql.Tests
     [Fact]
     public void Set_InvisibilityTimeout_ShouldThrowAnException_WhenGivenIntervalIsEqualToZero()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
       Assert.Throws<ArgumentException>(() => options.InvisibilityTimeout = TimeSpan.Zero);
     }
 
     [Fact]
     public void Set_InvisibilityTimeout_ShouldThrowAnException_WhenGivenIntervalIsNegative()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
       Assert.Throws<ArgumentException>(() => options.InvisibilityTimeout = TimeSpan.FromSeconds(-1));
     }
 
     [Fact]
     public void Set_InvisibilityTimeout_SetsTheValue()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
       options.InvisibilityTimeout = TimeSpan.FromSeconds(1);
       Assert.Equal(TimeSpan.FromSeconds(1), options.InvisibilityTimeout);
     }
@@ -63,21 +73,21 @@ namespace Hangfire.PostgreSql.Tests
     [Fact]
     public void Set_DistributedLockTimeout_ShouldThrowAnException_WhenGivenIntervalIsEqualToZero()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
       Assert.Throws<ArgumentException>(() => options.DistributedLockTimeout = TimeSpan.Zero);
     }
 
     [Fact]
     public void Set_DistributedLockTimeout_ShouldThrowAnException_WhenGivenIntervalIsNegative()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
       Assert.Throws<ArgumentException>(() => options.DistributedLockTimeout = TimeSpan.FromSeconds(-1));
     }
 
     [Fact]
     public void Set_DistributedLockTimeout_SetsTheValue()
     {
-      PostgreSqlStorageOptions options = new PostgreSqlStorageOptions();
+      PostgreSqlStorageOptions options = new();
       options.DistributedLockTimeout = TimeSpan.FromSeconds(1);
       Assert.Equal(TimeSpan.FromSeconds(1), options.DistributedLockTimeout);
     }
