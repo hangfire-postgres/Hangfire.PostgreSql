01a13d0306055e0a7ff89ae0492150b903e1a55a
Dmitry Vychikov <dmitry.vychikov@dsr-corporation.com>
2024-03-03 17:51:14 +0300

    [#348] Improve dequeue performance
    Create index for jobqueue table that puts NULLs in "fetchedat" column first for better de-queueing performance
    
    Signed-off-by: Dmitry Vychikov <dmitry.vychikov@dsr-corporation.com>

---
+++ src/Hangfire.PostgreSql/Scripts/Install.v22.sql
@@ -0,0 +1,13 @@
+﻿SET search_path = 'hangfire';
+
+DO $$
+BEGIN
+    IF EXISTS(SELECT 1 FROM "schema" WHERE "version"::integer >= 22) THEN
+        RAISE EXCEPTION 'version-already-applied';
+END IF;
+END $$;
+
+DROP INDEX IF EXISTS jobqueue_queue_fetchat_jobId;
+CREATE INDEX IF NOT EXISTS ix_hangfire_jobqueue_fetchedat_queue_jobid ON jobqueue USING btree (fetchedat nulls first, queue, jobid);
+
+RESET search_path;
