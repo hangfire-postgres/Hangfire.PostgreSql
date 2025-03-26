09f2c605008d653e2ba2235ad9d371411a5bd7ba
Miguel <10773024+miguelaristy@users.noreply.github.com>
2025-03-25 20:31:16 -0500

    Validate INCLUDE compatibility for index creation in PostgreSQL

--- src/Hangfire.PostgreSql/Scripts/Install.v23.sql
+++ src/Hangfire.PostgreSql/Scripts/Install.v23.sql
@@ -1,17 +1,9 @@
 SET search_path = 'hangfire';
 
 DO $$
-DECLARE v_version INTEGER;
 BEGIN
-    IF EXISTS (
-        SELECT 1 FROM information_schema.columns 
-        WHERE table_name = 'schema' AND column_name = 'version'
-    ) THEN
-        SELECT COALESCE(MAX("version")::integer, 0) INTO v_version FROM "schema";
-
-        IF v_version >= 23 THEN
-            RAISE EXCEPTION 'version-already-applied';
-        END IF;
+    IF EXISTS(SELECT 1 FROM "schema" WHERE "version"::integer >= 23) THEN
+        RAISE EXCEPTION 'version-already-applied';
     END IF;
 END $$;
 
@@ -19,12 +11,11 @@ DROP INDEX IF EXISTS ix_hangfire_job_statename_is_not_null;
 
 DO $$
 BEGIN
-    BEGIN
+    IF current_setting('server_version_num')::int >= 110000 THEN
         EXECUTE 'CREATE INDEX ix_hangfire_job_statename_is_not_null ON job USING btree(statename) INCLUDE (id) WHERE statename IS NOT NULL';
-    EXCEPTION
-        WHEN others THEN
-            EXECUTE 'CREATE INDEX ix_hangfire_job_statename_is_not_null ON job USING btree(statename) WHERE statename IS NOT NULL';
-    END;
+    ELSE
+        EXECUTE 'CREATE INDEX ix_hangfire_job_statename_is_not_null ON job (statename) WHERE statename IS NOT NULL';
+    END IF;
 END $$;
 
 RESET search_path;
