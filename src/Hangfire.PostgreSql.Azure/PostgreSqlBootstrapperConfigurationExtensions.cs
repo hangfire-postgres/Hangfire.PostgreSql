ccc8d3df9480bc767f7e65cca70aa5dd21c116cd
alex_maiereanu <alex.maiereanu@3sstudio.com>
2025-04-12 15:51:41 +0300

    * created a new assembly in order to isolate the requirements for Azure Tokens: Hangfire.PostgreSql.Azure * implemented a factory so that users can easily plugin

---
+++ src/Hangfire.PostgreSql.Azure/PostgreSqlBootstrapperConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire.Annotations;
+using Hangfire.PostgreSql.Azure.Factories;
+using Npgsql;
+
+namespace Hangfire.PostgreSql.Azure
+{
+  public static class PostgreSqlBootstrapperConfigurationExtensions
+  {
+    /// <summary>
+    ///   Tells the bootstrapper to use PostgreSQL as a job storage  with the given options
+    /// </summary>
+    /// <param name="configuration">Configuration</param>
+    /// <param name="connectionString">Connection string</param>
+    /// <param name="dataSourceBuilderSetup">You have here the opportunity to override the datasource builder, including the password provider</param>
+    public static IGlobalConfiguration<PostgreSqlStorage> UseAzurePostgreSqlStorage(
+      this IGlobalConfiguration configuration,
+      string connectionString,
+      PostgreSqlStorageOptions options,
+      [CanBeNull] Action<NpgsqlDataSourceBuilder>? dataSourceBuilderSetup = null)
+    {
+      return configuration.UsePostgreSqlStorage(c => c.UseConnectionFactory(new AzureNpgsqlConnectionFactory(connectionString, options, dataSourceBuilderSetup)), options);
+    }
+  }
+
+}
