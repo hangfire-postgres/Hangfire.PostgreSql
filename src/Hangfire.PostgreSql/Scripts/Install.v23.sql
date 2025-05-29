c9ee54aa5cd7741476880081f673b9b3a5029ce0
sake12 <sake12@gmail.com>
2025-05-29 11:49:34 +0200

    Use EXECUTE for index creation in PostgreSQL for server version check

--- src/Hangfire.PostgreSql/Scripts/Install.v23.sql
+++ src/Hangfire.PostgreSql/Scripts/Install.v23.sql
@@ -12,7 +12,7 @@ DROP INDEX IF EXISTS ix_hangfire_job_statename_is_not_null;
 DO $$
 BEGIN
     IF current_setting('server_version_num')::int >= 110000 THEN
-        CREATE INDEX ix_hangfire_job_statename_is_not_null ON job USING btree(statename) INCLUDE (id) WHERE statename IS NOT NULL;
+        EXECUTE 'CREATE INDEX ix_hangfire_job_statename_is_not_null ON job USING btree(statename) INCLUDE (id) WHERE statename IS NOT NULL';
     ELSE
 		CREATE INDEX ix_hangfire_job_statename_is_not_null ON job USING btree(statename) WHERE statename IS NOT NULL;
     END IF;
